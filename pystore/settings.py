b60e1b8735570d6700b825b1bd407395d5095156
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-12-29 16:05:08 -0300

    Pagination, models and products

--- pystore/settings.py
+++ pystore/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "users.apps.UsersConfig",
+    "pages.apps.PagesConfig",
+    "products.apps.ProductsConfig",
 ]
 
 MIDDLEWARE = [
@@ -125,6 +127,12 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 AUTH_USER_MODEL = "users.User"
+
+
+# MEDIA
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
